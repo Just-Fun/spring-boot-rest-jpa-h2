15984078f36803ed888250183c94d461451e4990
Sergii Zagryvyi <serzhcello@gmail.com>
2018-10-26 00:27:29 +0300

    add controller tests

---
+++ src/test/java/com/serzh/example/WebMockTest.java
@@ -0,0 +1,47 @@
+package com.serzh.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.serzh.example.controller.EmployeeRestController;
+import com.serzh.example.entity.Employee;
+import com.serzh.example.service.EmployeeService;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(EmployeeRestController.class)
+public class WebMockTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService service;
+
+    @Test
+    public void greetingShouldReturnMessageFromService() throws Exception {
+        List<Employee> employees = new ArrayList<>();
+        Employee employee = new Employee();
+        employee.setId(9L);
+        employee.setName("Vasilii");
+        employees.add(employee);
+
+        when(service.retrieveEmployees()).thenReturn(employees);
+        this.mockMvc.perform(get("/api/employees")).andDo(print()).andExpect(status().isOk())
+            .andExpect(content().string(containsString("Vasilii")));
+    }
+}
